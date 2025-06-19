ba74d87d12e35fdb14695f6b892bd775b1b69ea7
mysthic <mouloud.messd@gmail.com>
2025-06-19 20:56:58 +0200

    bugfix/ correction du formattage des facture pdf apres export, symbole euro qui s'affiche pas

--- lib/Utils/Utils.dart
+++ lib/Utils/Utils.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
@@ -6,6 +7,10 @@ import 'package:reewayyfacture/DB/db.config.dart';
 class Utils {
   static Future<void> exporterFacture(int factureId) async {
     final db = await DatabaseHelper.instance.database;
+
+    final fontData =
+        await rootBundle.load('assets/fonts/Roboto-VariableFont_wdth,wght.ttf');
+    final ttf = pw.Font.ttf(fontData.buffer.asByteData());
 
     final facture = (await db.query(
       'facture',
@@ -27,11 +32,14 @@ class Utils {
         build: (context) => pw.Column(
           crossAxisAlignment: pw.CrossAxisAlignment.start,
           children: [
-            pw.Text('Facture', style: pw.TextStyle(fontSize: 24)),
+            pw.Text('Facture', style: pw.TextStyle(font: ttf, fontSize: 24)),
             pw.SizedBox(height: 16),
-            pw.Text('Client : ${facture['client_name']}'),
-            pw.Text('Email : ${facture['client_email']}'),
-            pw.Text('Date : ${facture['date']}'),
+            pw.Text('Client : ${facture['client_name']}',
+                style: pw.TextStyle(font: ttf)),
+            pw.Text('Email : ${facture['client_email']}',
+                style: pw.TextStyle(font: ttf)),
+            pw.Text('Date : ${facture['date']}',
+                style: pw.TextStyle(font: ttf)),
             pw.SizedBox(height: 24),
             pw.Table.fromTextArray(
               headers: ['Article', 'Quantité', 'PU', 'Total HT'],
@@ -43,14 +51,16 @@ class Utils {
                         '${a['total_ht']}€',
                       ])
                   .toList(),
+              headerStyle: pw.TextStyle(font: ttf),
+              cellStyle: pw.TextStyle(font: ttf),
             ),
             pw.SizedBox(height: 24),
-            pw.Text('Total TTC : ${facture['total_ttc']}€'),
+            pw.Text('Total TTC : ${facture['total_ttc']}€',
+                style: pw.TextStyle(font: ttf)),
           ],
         ),
       ),
     );
-
     await Printing.layoutPdf(
       onLayout: (format) async => doc.save(),
     );
